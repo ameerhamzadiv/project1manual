ebe028c22d4b05cfb2fcbc99e0ca5e1dc4a30ebd
Ameer Hamza <ameer.hamza.div@gmail.com>
2025-11-25 02:38:31 +0500

    Upload project on main

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Attemp 1 
+          <br/>
+          Project 1 Manual
+          <br/>
+          From Github Action: Test, Build, Docker Image
         </p>
         <a
           className="App-link"
